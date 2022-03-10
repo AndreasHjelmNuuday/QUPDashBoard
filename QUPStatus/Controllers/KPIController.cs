a3db82b9b2886feb605ea21ae73d77845c6758a0
AndreasHjelmNuuday <andhj@nuuday.dk>
2022-03-10 11:19:55 +0100

    KPI Controller added

---
+++ QUPStatus/Controllers/KPIController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QUPStatus.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DotJira;
+
+namespace QUPStatus.Controllers
+{
+    public class KPIController : Controller
+    {
+        private readonly ILogger<KPIController> _logger;
+        
+        public KPIController(ILogger<KPIController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string[] issues)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
